b54c174c39c56a8af1721eb0bf0326b2390cc5ec
Anouk Shih <47846927+anouk-shih@users.noreply.github.com>
2024-07-10 20:56:56 +0800

    init testing libs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { defineConfig as defineVitestConfig } from "vitest/config";
 
-// https://vitejs.dev/config/
+import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
+import react from "@vitejs/plugin-react";
+
+// For general Vite configuration
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), viteCommonjs()],
+});
+
+// For Vitest specific configuration
+export const testConfig = defineVitestConfig({
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./tests/setup.ts",
+  },
+});
